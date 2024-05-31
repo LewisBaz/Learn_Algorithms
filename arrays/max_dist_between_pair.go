610457e21506ea092fdc6e861a4237f622091fa3
Lewis <lev.bazhkov@aisa.ru>
2024-05-31 18:47:17 +0300

    new good version of binary search

--- arrays/max_dist_between_pair.go
+++ arrays/max_dist_between_pair.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+// NOT COMPLETED
+
 // You are given two non-increasing 0-indexed integer arrays nums1​​​​​​ and nums2​​​​​​.
 
 // A pair of indices (i, j), where 0 <= i < nums1.length and 0 <= j < nums2.length, is valid if both i <= j and nums1[i] <= nums2[j]. The distance of the pair is j - i​​​​.
@@ -20,8 +22,12 @@ func MaxDistance(nums1 []int, nums2 []int) int {
 	n1 := 0
 	n2 := len(nums2) - 1
 
+	counter := 0
+
 	for n1 <= n2 {
+		counter += 1
 		if len(nums1) <= n1 || len(nums2) <= n2 {
+			fmt.Println(counter)
 			fmt.Println(valid)
 			return maxDistance
 		}
@@ -31,6 +37,10 @@ func MaxDistance(nums1 []int, nums2 []int) int {
 			diff := n2 - n1
 			if diff > maxDistance {
 				maxDistance = diff
+				// if n2 == len(nums2) - 1 {
+				// 	fmt.Println(counter)
+				// 	return maxDistance
+				// }
 			}
 		}
 		if n1 == n2 || nums2[n2-1] == nums2[n2] {
@@ -42,6 +52,7 @@ func MaxDistance(nums1 []int, nums2 []int) int {
 	}
 
 	fmt.Println(valid)
+	fmt.Println(counter)
 
 	return maxDistance
 }
--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -1,26 +1,17 @@
 package binarysearch
 
 func Search(nums []int, target int) int {
-	origin := nums
-	counter := 0
-	var index int = -1
-	for index == -1 {
-		length := len(origin)
-		left := origin[:length/2]
-		right := origin[length/2:]
-		if len(left) != 0 && target > left[len(left)-1] {
-			origin = right
-			counter += len(left)
-		} else if len(left) != 0 && target < left[len(left)-1] {
-			origin = left
-		} else if len(left) != 0 {
-			counter += len(left)
-			index = counter - 1
-		} else if counter == len(nums) - 1 && nums[counter] == target {
-			index = counter
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
 		} else {
-			return index
+			right = mid - 1
 		}
 	}
-	return index
+	return -1
 }
