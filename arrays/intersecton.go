a3fc28e78243eddebd9fdc12ff9bf94ea36cdce3
Lewis <lev.bazhkov@aisa.ru>
2024-06-01 04:53:13 +0300

    intersection of two arrays

---
+++ arrays/intersecton.go
@@ -0,0 +1,28 @@
+package arrays
+
+func Intersect(nums1 []int, nums2 []int) []int {
+	result := make([]int, 0)
+	inters := make(map[int]int,0)
+	var small []int
+	var big []int
+
+	if len(nums1) > len(nums2) {
+		small = nums2
+		big = nums1
+	} else {
+		small = nums1
+		big = nums2
+	}
+
+	for i := 0; i < len(small); i++ {
+		inters[small[i]] += 1
+	}
+	for _, v := range big {
+		if inters[v] != 0 {
+			result = append(result, v)
+			inters[v] -= 1
+		}
+	}
+
+	return result
+}
